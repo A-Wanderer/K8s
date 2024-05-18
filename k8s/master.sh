e89309948e4eac56bc36ab64afc62310ae6e6d27
Nanno <675502062@qq.com>
2024-05-18 14:45:08 +0800

    Update master.sh

--- k8s/master.sh
+++ k8s/master.sh
@@ -5,6 +5,7 @@ bash install_k8s.sh
 bash start_k8s_master.sh
 cd sre
 bash install_network_on_master.sh
-echo "还需要："
+echo "还需要等worker加入后执行："
 echo "cd k8s-gpu && bash install_operator_on_master.sh"
+echo "cd ../tf && bash install_tf_operator.sh"
 echo "cd ../dashboard && bash submit_on_master.sh"
